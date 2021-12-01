1f7d1bd9ade12ce557a048ac9849bd56e71aa735
Celeo <mattboulanger@fastmail.com>
2021-12-01 09:01:05 -0800

    Day 01 part b

--- 2021/src/day01/part_b.rs
+++ 2021/src/day01/part_b.rs
@@ -1,1 +1,17 @@
-pub fn run() {}
+pub fn run() {
+    let values: Vec<_> = std::fs::read_to_string("src/day01/input.txt")
+        .unwrap()
+        .split_terminator('\n')
+        .map(|l| l.parse::<usize>().unwrap())
+        .collect();
+    let result = values
+        .iter()
+        .take(values.len() - 2)
+        .enumerate()
+        .map(|(i, v)| v + values.get(i + 1).unwrap() + values.get(i + 2).unwrap())
+        .fold((usize::MAX, 0), |acc, value| {
+            (value, if value > acc.0 { acc.1 + 1 } else { acc.1 })
+        })
+        .1;
+    log::info!("Result: {}", result); // 1633
+}
