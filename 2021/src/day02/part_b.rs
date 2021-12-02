683faf002371bc63fb6c5a325e153fe4384c6073
Celeo <mattboulanger@fastmail.com>
2021-12-01 21:54:17 -0800

    day 02 part b

--- 2021/src/day02/part_b.rs
+++ 2021/src/day02/part_b.rs
@@ -1,1 +1,17 @@
-pub fn run() {}
+pub fn run() {
+    let (x, y, _aim) = std::fs::read_to_string("src/day02/input.txt")
+        .unwrap()
+        .split_terminator('\n')
+        .fold((0, 0, 0), |(x, y, aim), line| {
+            let mut parts = line.split_whitespace();
+            let action = parts.next().unwrap();
+            let amount: usize = parts.next().unwrap().parse().unwrap();
+            match action {
+                "forward" => (x + amount, y + (amount * aim), aim),
+                "down" => (x, y, aim + amount),
+                "up" => (x, y, aim - amount),
+                _ => (x, y, aim),
+            }
+        });
+    log::info!("Result: {}", x * y); // 1845455714
+}
