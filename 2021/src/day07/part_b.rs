e5557c1931cc122bafbdd61e42b65a53d4219f57
Celeo <mattboulanger@fastmail.com>
2021-12-06 21:27:32 -0800

    day 07 part b

--- 2021/src/day07/part_b.rs
+++ 2021/src/day07/part_b.rs
@@ -1,1 +1,22 @@
-pub fn run() {}
+fn move_to(positions: &[u64], to: i64) -> u64 {
+    positions
+        .iter()
+        .map(|&n| {
+            let delta = (n as i64 - to).abs();
+            (delta * (delta + 1)) / 2
+        })
+        .sum::<i64>() as u64
+}
+
+pub fn run() {
+    let data = std::fs::read_to_string("src/day07/input.txt").unwrap();
+    let positions: Vec<u64> = data.trim().split(',').map(|s| s.parse().unwrap()).collect();
+    let mut least = (0, u64::MAX);
+    for to in *positions.iter().min().unwrap()..*positions.iter().max().unwrap() + 1 {
+        let cost = move_to(&positions, to as i64);
+        if cost < least.1 {
+            least = (to, cost);
+        }
+    }
+    println!("Result: {} (to {})", least.1, least.0);
+}
