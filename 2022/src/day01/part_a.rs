e901b40167fe0568c00aba31da81d1436eecf195
Celeo <mattboulanger@fastmail.com>
2022-12-01 12:58:56 -0800

    2022 01a

---
+++ 2022/src/day01/part_a.rs
@@ -0,0 +1,13 @@
+use anyhow::anyhow;
+use log::info;
+
+pub fn run() -> anyhow::Result<()> {
+    let text = std::fs::read_to_string("src/day01/input.txt")?;
+    let result: u64 = text
+        .split("\n\n")
+        .map(|line| line.split("\n").filter_map(|s| s.parse::<u64>().ok()).sum())
+        .max()
+        .ok_or(anyhow!("Could not find sum"))?;
+    info!("{result}");
+    Ok(())
+}
