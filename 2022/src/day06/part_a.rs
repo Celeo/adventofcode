0f33e24f80e51e3f80588cb28d756da0d82aa48f
Celeo <mattboulanger@fastmail.com>
2022-12-05 21:38:51 -0800

    Clippy

--- 2022/src/day06/part_a.rs
+++ 2022/src/day06/part_a.rs
@@ -4,7 +4,13 @@ use log::info;
 
 pub fn run(text: &str) {
     for index in 4..text.len() {
-        if HashSet::<char>::from_iter(text[index - 4..index].chars().into_iter()).len() == 4 {
+        if text[index - 4..index]
+            .chars()
+            .into_iter()
+            .collect::<HashSet<char>>()
+            .len()
+            == 4
+        {
             info!("{index}");
             return;
         }
--- 2022/src/day06/part_b.rs
+++ 2022/src/day06/part_b.rs
@@ -4,7 +4,13 @@ use log::info;
 
 pub fn run(text: &str) {
     for index in 14..text.len() {
-        if HashSet::<char>::from_iter(text[index - 14..index].chars().into_iter()).len() == 14 {
+        if text[index - 14..index]
+            .chars()
+            .into_iter()
+            .collect::<HashSet<char>>()
+            .len()
+            == 14
+        {
             info!("{index}");
             return;
         }
