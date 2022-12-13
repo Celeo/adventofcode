f6519a66142e349a7c258bd3570b6ab4cb42d16e
Celeo <mattboulanger@fastmail.com>
2022-12-12 20:14:27 -0800

    2022 07a WIP

--- 2022/src/day07/part_a.rs
+++ 2022/src/day07/part_a.rs
@@ -1,22 +1,59 @@
 use std::collections::HashMap;
 
-use itertools::Itertools;
+#[derive(Debug, Clone)]
+enum Item {
+    File(String, usize),
+    Directory(String, Vec<Item>),
+}
 
-pub fn run(text: &str) {
-    let mut fs: HashMap<String, Vec<(String, usize)>> = HashMap::new();
+fn insert(parent: &mut Item, path: &str, new: &Item) {
+    if path.is_empty() {
+        if let Item::Directory(_, dir_items) = parent {
+            dir_items.push(new.clone());
+        }
+        return;
+    }
+    let mut path_parts = path.split('/');
+    let next_dir = path_parts.nth(1).unwrap();
+    let remaining_path = {
+        let s = path_parts.collect::<String>();
+        if s.is_empty() {
+            s
+        } else {
+            format!("/{s}")
+        }
+    };
+    if let Item::Directory(_, dir_items) = parent {
+        if let Some(next) = dir_items.iter_mut().find(|d| match d {
+            Item::Directory(dir_name, _) => dir_name == next_dir,
+            Item::File(..) => false,
+        }) {
+            insert(next, &remaining_path, new);
+        } else {
+            let next = Item::Directory(next_dir.to_owned(), Vec::new());
+            dir_items.push(next);
+            insert(dir_items.last_mut().unwrap(), &remaining_path, new);
+        }
+    }
+}
+
+fn construct(text: &str) -> Item {
+    let mut root = Item::Directory(String::new(), Vec::new());
     let mut pwd = String::new();
 
     for line in text.split_terminator('\n') {
         if line.starts_with("$ cd") {
             if line.chars().nth(5).unwrap() == '/' {
-                pwd = String::from("/");
+                pwd = String::new();
                 continue;
             }
             let path = line.chars().skip(5).collect::<String>();
             if path == ".." {
-                pwd = pwd.split('/').take(pwd.matches('/').count() - 1).join("/") + "/";
+                let mut v = pwd.split('/').collect::<Vec<_>>();
+                let _ = v.pop();
+                pwd = v.join("/");
             } else {
-                pwd = pwd + &path + "/";
+                pwd = pwd + "/" + &path;
             }
             continue;
         }
@@ -24,20 +61,31 @@ pub fn run(text: &str) {
             continue;
         }
         let (size, file) = line.split_once(' ').unwrap();
-        let in_path = fs.entry(pwd.clone()).or_insert_with(Vec::new);
-        in_path.push((file.to_string(), size.parse().unwrap()));
+        insert(
+            &mut root,
+            &pwd,
+            &Item::File(file.to_string(), size.parse().unwrap()),
+        );
     }
 
-    let matching: Vec<_> = fs
-        .keys()
-        .filter(|&path| {
-            // TODO need to count nested directories as well
-            let in_dir = fs.get(path).unwrap().iter().map(|(_, s)| *s).sum::<usize>();
-            in_dir > 100_000
-        })
-        .collect();
+    root
+}
 
-    dbg!(matching);
+fn find_size(root: &Item, max_size: usize) -> Vec<usize> {
+    let mut sizes: HashMap<String, usize> = HashMap::new();
 
-    todo!()
+    if let Item::Directory(_, dir_items) = root {
+        //
+    }
+
+    sizes
+        .iter()
+        .filter(|(_key, &value)| value <= max_size)
+        .map(|(_key, value)| *value)
+        .collect()
+}
+
+pub fn run(text: &str) {
+    let fs = construct(text);
+    let dirs = find_size(&fs, 100_000);
 }
