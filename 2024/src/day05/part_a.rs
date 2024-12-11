16d891db1e416d813ce7f4041ffc3486c498cbf6
Celeo <mattboulanger@fastmail.com>
2024-12-10 16:38:33 -0800

    Day 05 part a WIP

--- 2024/src/day05/part_a.rs
+++ 2024/src/day05/part_a.rs
@@ -1,3 +1,23 @@
 pub fn run(text: &str) {
-    // TODO
+    let rules: Vec<_> = text
+        .split_terminator('\n')
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            let parts: Vec<u32> = line.split('|').map(|s| s.parse().unwrap()).collect();
+            (*parts.get(0).unwrap(), *parts.get(1).unwrap())
+        })
+        .collect();
+
+    let updates: Vec<Vec<u32>> = text
+        .split_terminator('\n')
+        .skip_while(|line| !line.is_empty())
+        .skip(1)
+        .map(|line| line.split(',').map(|s| s.parse().unwrap()).collect())
+        .collect();
+
+    for update in updates {
+        for n in update {
+            //
+        }
+    }
 }
